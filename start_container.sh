725683659d7ba4ce3337f3d733f506342d3e7a2d
tbehner <behner@cs.uni-bonn.de>
2017-10-04 11:09:40 +0200

    Update name of mass server for demo

--- start_container.sh
+++ start_container.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-docker run -e MASS_API_KEY=IjU5ZDM3Yzc0NmFlY2RmN2MzNGIzYjAyMiI.WhU92Ly9Tq4fc63l0qKfl944Jj4 -e MASS_SERVER=http://172.17.0.1:8000/api/ -ti mass_sys_unzipper
+docker run -e MASS_API_KEY=IjU5ZDM3Yzc0NmFlY2RmN2MzNGIzYjAyMiI.WhU92Ly9Tq4fc63l0qKfl944Jj4 -e MASS_SERVER=http://mass-server.lan:8000/api/ -ti mass_sys_unzipper
